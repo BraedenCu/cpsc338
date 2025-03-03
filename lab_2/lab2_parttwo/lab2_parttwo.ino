53cf6444d640f8dbe0be1f6cf19b05163158d31e
pradyunSolai <pradyunsolai@gmail.com>
2025-03-03 18:56:41 -0500

    Implemented with no shift registers

--- lab_2/lab2_parttwo/lab2_parttwo.ino
+++ lab_2/lab2_parttwo/lab2_parttwo.ino
@@ -101,32 +101,63 @@ int matrices[10][7][5] = {
   }
 };
 
+// Arduino pins that correspond to dot-matrix rows
+// These pins need to be high for the LED to turn on
+int row_pins[7] = {2, 3, 4, 5, 6, 7, 8};
+
+// Arduino pins that correspond to dot-matrix rows
+// These pins need to be low for the LED to turn on
+int col_pins[5] = {9, 10, 11, 12, 13};
+
+// Write to a row given the columns that should be turned on in that row
+// for example, if row = 0 and cols = {0, 1, 1 1, 0}, then the top row is set to off, on, on, on, off
+void write_to_row(int row, int cols[]) {
+  // set row pins
+  for (int i = 0; i < 7; i++) {
+    digitalWrite(row_pins[i], row == i);
+  }
+
+  // Serial.print("Printing: ");
+  // set column pins
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(col_pins[i], !cols[i]);
+    // Serial.print(cols[i]);
+  }
+  // Serial.print(" to row ");
+  // Serial.println(row);
+
+
+}
+
 
 void display_digit(int digit) {
   // display the digit
   Serial.println(digit);
   for (int i = 0; i < 7; i++) {
-    for (int j = 0; j < 5; j++) {
-      // display the digit
-      if (matrices[digit][i][j]) {
-        Serial.print("█");
-      } else {
-        Serial.print(" ");
-      }
-    }
-    Serial.println();
+    write_to_row(i, matrices[digit][i]);
+    delay(1);
   }
 }
 
 void setup() {
   Serial.begin(9600);
-  
+
+  // set all row pins to output
+  for (int i = 0; i < 7; i++) {
+    pinMode(row_pins[i], OUTPUT);
+  }
+
+  // set all column pins to output
+  for (int i = 0; i < 5; i++) {
+    pinMode(col_pins[i], OUTPUT);
+  }
 }
 
 
 void loop() {
   for (int i = 0; i < 10; i++) {
-    display_digit(i);
-    delay(1000);
+    for (int j = 0; j < 50; j++) {
+      display_digit(i);
+    }
   }
 }
