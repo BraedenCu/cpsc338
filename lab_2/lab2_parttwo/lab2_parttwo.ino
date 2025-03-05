ae30b18066cfed8dff60c7059fceac0bb72881b2
pradyunSolai <pradyunsolai@gmail.com>
2025-03-05 17:10:51 -0500

    Completed part 2

--- lab_2/lab2_parttwo/lab2_parttwo.ino
+++ lab_2/lab2_parttwo/lab2_parttwo.ino
@@ -1,3 +1,4 @@
+// Font definition 
 int matrices[10][7][5] = {
   // digit 0
   {
@@ -101,55 +102,73 @@ int matrices[10][7][5] = {
   }
 };
 
+// define pin values
 #define SER 4
 #define RCLK 5
 #define SRCLK 6
 
-// Write to a row given the columns that should be turned on in that row
-// for example, if row = 0 and cols = {0, 1, 1 1, 0}, then the top row is set to off, on, on, on, off
-void write_to_row(int row, int cols[]) {
-  digitalWrite(RCLK, 0);
-
-  for (int i = 4; i >= 0; i--) {
-    digitalWrite(SRCLK, 0);
-    delay(1);
-    digitalWrite(SER, cols[i]);
-    delay(1);
-    digitalWrite(SRCLK, 1);
+// Basically the same as digitalWrite
+// Write value (1 or 0) to an output pin
+// pin must be initialized to output for this to work
+void write_to_pin(int pin, int value) {
+  if (value) {
+    PORTD |= (1 << pin);
+  } else {
+    PORTD &= ~(1 << pin);
   }
-
-  for (int i = 6; i >= 0; i--) {
-    digitalWrite(SRCLK, 0);
-    delay(1);
-    digitalWrite(SER, i != row);
-    delay(1);
-    digitalWrite(SRCLK, 1);
-  }
-
-  digitalWrite(RCLK, 1);
 }
 
+// Set an LED matrix row to the values in cols
+// for example, if row = 0 and cols = {0, 1, 1, 1, 0}, then the top row is set to off, on, on, on, off
+//                                                     and the rest of the rows are turned off
+void write_to_row(int row, int cols[]) {
+  // Set RCLK pin on shift register to 0
+  // this way, it does not update the register output pins until RCLK goes high
+  write_to_pin(RCLK, 0);
 
+  // Shift all of the row values
+  // Only want to turn on the matrix row corresponding to the variable row
+  // A row needs to be set to 1 for the LEDs to turn on
+  for (int i = 0; i < 7; i++) {
+    write_to_pin(SRCLK, 0);
+    write_to_pin(SER, i == row);
+    write_to_pin(SRCLK, 1);
+  }
+
+  // Shift all of the column values
+  // The column needs to be set to 0 for the LED to turn on
+  for (int i = 0; i < 5; i++) {
+    write_to_pin(SRCLK, 0);
+    write_to_pin(SER, !cols[i]);
+    write_to_pin(SRCLK, 1);
+  }
+
+  // Toggle the RCLK pin so that the register outputs are set synchronously
+  write_to_pin(RCLK, 1);
+}
+
+// display a digit for one cycle
 void display_digit(int digit) {
-  // display the digit
-  Serial.println(digit);
+  // Loop through the rows really fast and disply them to give the illusion that all the LEDs are on
   for (int i = 0; i < 7; i++) {
     write_to_row(i, matrices[digit][i]);
+    delay(2);
   }
 }
 
 void setup() {
   Serial.begin(9600);
 
-  pinMode(SER, OUTPUT);
-  pinMode(RCLK, OUTPUT);
-  pinMode(SRCLK, OUTPUT);
+  DDRD |= (1 << SER);   // set SER pin as output
+  DDRD |= (1 << RCLK);  // set RCLK pin as output
+  DDRD |= (1 << SRCLK); // set SRCLK pin as output
 }
 
 
 void loop() {
+  // Iterate through the numbers and display them
   for (int i = 0; i < 10; i++) {
-    Serial.println(i);
+    // Display each digit for 50 cycles
     for (int j = 0; j < 50; j++) {
       display_digit(i);
     }
